13e232fca0e2b53dffc6ba424fc17509dce48284
Omkar Singh <omkarsingh52719@gmail.com>
2026-08-21 11:14:36 +0530

    File added

--- spark.java
+++ spark.java
@@ -2,4 +2,7 @@ class spark{
     public static void main(String[] args){
         System.out.println("Hello Buddy");
     }
+    public void m1(){
+        System.out.println("Omkar Singh");
+    }
 }
